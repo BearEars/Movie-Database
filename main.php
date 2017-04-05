3d9842c455374c514b9dba879b1141699d4fe9bc
BearEars <taylor.ecton@gmail.com>
2017-04-05 01:36:19 -0400

    wrote test php code in main.php

--- main.php
+++ main.php
@@ -1,3 +1,14 @@
 <html>
+  <head>
+    <title>Main Page</title>
+  </head>
+  <body>
+    <?php
+      if (isset($_POST["Fname"])
+      {
+        echo "Test worked.";
+      }
+    ?>
+  </body>
 </html>
 
