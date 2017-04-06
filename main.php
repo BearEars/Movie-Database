3934bd896b7461aa44b51c334a33a4bd90ca81d7
BearEars <taylor.ecton@gmail.com>
2017-04-06 01:21:42 -0400

    set up skeleton and some basic functions in main.php. To see it in action go to cs.uky.edu/~twecto2/HTML/CS405/Movie-Database/user_registration.html

--- main.php
+++ main.php
@@ -1,14 +1,68 @@
 <html>
-  <head>
-    <title>Main Page</title>
-  </head>
-  <body>
-    <?php
-//      if (isset($_POST["Fname"])
-//      {
-        echo "Test worked.";
-//      }
-    ?>
-  </body>
+<head>
+	<title>Main Page</title>
+</head>
+
+<body>
+<?php
+	if (fromUserReg($_POST)) {
+		echo "User registration form filled out.";
+	}
+	elseif (fromHome($_POST)) {
+		echo "User login from main page.";
+	}
+	else {
+		echo "<h1>Access denied.</h1>";
+	}
+
+// ------------------ USER LOGIN FROM MAIN.HTML -------------------------------
+function fromHome($postVars) {
+	/* Function checks if post variables from home.html are set;
+           returns true if they are, false otherwise */
+	$varsSet = True; 
+        if (!isset($postVars["Username"]))
+                $varsSet = False;
+        if (!isset($postVars["Password"]))
+                $varsSet = False;
+	return $varsSet;
+}
+
+function login() {
+	// log an existing user in; check username/password combo
+}
+// ----------------------------------------------------------------------------
+
+// ------------- USER REGISTRATION FROM USER_REGISTRATION.HTML ----------------
+function fromUserReg($postVars) {
+	/* Function checks if post variables from user_registration.html
+	   are set; returns true if they are, false otherwise */
+	$varsSet = True;
+	if (!isset($postVars["Username"]))
+		$varsSet = False;
+	if (!isset($postVars["Password"]))
+		$varsSet = False;
+	if (!isset($postVars["Fname"]))
+		$varsSet = False;
+	if (!isset($postVars["Mname"]))
+		$varsSet = False;
+	if (!isset($postVars["Lname"]))
+		$varsSet = False;
+	if (!isset($postVars["month"]))
+		$varsSet = False;
+	if (!isset($postVars["day"]))
+		$varsSet = False;
+	if (!isset($postVars["year"]))
+		$varsSet = False;
+	if (!isset($postVars["Gender"]))
+		$varsSet = False;
+	return $varsSet;
+}
+
+function registerUser() {
+	// Do the things to create a new user in database
+}
+// ----------------------------------------------------------------------------
+?>
+</body>
 </html>
 
