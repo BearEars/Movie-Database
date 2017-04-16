a1829e1d52d6a6f4d42c08784fd536902bf59a9f
jingwenhuang <jhu272@g.uky.edu>
2017-04-16 16:52:22 -0400

    Update submittag.php
    change the function for adding tag

--- submittag.php
+++ submittag.php
@@ -32,7 +32,7 @@
     $tag=$_POST["tag_type"];
     $movieID = $_POST["movie_id"];
 
-    insertTags($link,$movieID,$tag);
+    addTag($link,$movieID,$tag);
     echo "Tags successfully added!<br>";
     echo "<a href=\"main.php\">Return to Search</a>";
 ?>
